eb323d78ce7de460970a2348810b6d865df0ca15
franciscoidalgo <49107244+franciscoidalgo@users.noreply.github.com>
2020-06-21 22:57:12 -0300

    Prueba again

--- script.js
+++ script.js
@@ -68,3 +68,19 @@ function cargarTxt(cual) {
     });
 
   }
+function cambiaContenido() {
+            var xhr = new XMLHttpRequest();
+            xhr.onreadystatechange = function () {
+                if (this.readyState == 4 && this.status == 200) {
+                    document.getElementById("info").innerHTML = this.responseText;
+                }
+            };
+            /* .open: especifica la solicitud
+             - GET/POST.
+             - Archivo: txt, php, xml, json, etc.
+             - true/false: método de envío. */
+            xhr.open("GET", "holamundo.txt", true);
+            /* .send: envía la solicitud al servidor.
+                Si utilizamos POST debemos pasar los datos por parámetro */
+            xhr.send();
+        }
